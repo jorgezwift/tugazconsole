36301eab6a83f140c681ef0ddcf4d90b02dd07b4
jorgezwift <72138400+jorgezwift@users.noreply.github.com>
2021-01-08 11:57:25 +0100

    Update rider.js
    add recovering levels

--- rider.js
+++ rider.js
@@ -34,14 +34,20 @@ class Rider {
 		//W'Bal
 		this.wprime = -1;
 		this.cp = -1;
+		this.div = "espresso";
+		this.factorDiff = 1;
 		
 		this.sampleTime = 0;
 		this.oneSsamples = [];
 		this.w_balV = 0;
 		
 		this.sn = 0;
-		this.ln = 0;
+		this.sn1 = 0;
+		this.sn2 = 0;
+		this.sn3 = 0;
 		this.prev_watt = 0;
+		
+		this.rest_rate = 0;
 		
 
 	 }
@@ -52,11 +58,31 @@ class Rider {
   }
   
   
-  tau() {
-	  //Tau = W' / CP
-	  var tauV = this.wprime*1000 / this.cp;
-	  return tauV;
-	  
+	tau(pexpN,modePar) {
+		var rest = 280;
+		if(this.div=="frappe")
+			rest = 250;
+		else if(this.div=="latte")
+			rest = 220;
+			
+		var modeInner = this.mode;
+		if(typeof modePar != 'undefined')
+			modeInner = modePar;
+			
+		var tauVC1 = this.wprime*1000;
+		if(modeInner == 3){
+			tauVC1 = Math.floor((this.wprime*1000 / (this.cp))*this.factorDiff);
+		}else if(modeInner == 2){
+			tauVC1 = Math.floor((this.wprime*1000 / (this.cp*0.4))*this.factorDiff);
+		}else if(modeInner == 1){
+			tauVC1 = Math.floor((this.wprime*1000 / (this.cp*0.2))*this.factorDiff);
+		}else{
+			if(rest>this.cp)
+				rest = this.cp*0.025;
+		tauVC1 = Math.floor((this.wprime*1000 / (this.cp-rest))*this.factorDiff);
+	}
+	
+	return tauVC1;
   }
   
 	w_bal(timediff, totaltime){
@@ -101,15 +127,37 @@ class Rider {
 	  wAVG = wAVG/this.oneSsamples.length;
 	  
 	  var pexp = wAVG-this.cp;
-	  if(pexp<0)
-		pexp = 0;
+	  var pexpN = this.cp-wAVG;
+	  this.mode = 0;
+	  if(pexp<0){
+		pexp=0;
+	  }else{
+		this.mode = -1;
+		pexpN=1;  
+	  }
+		
+		var snCur = 0;
+		if(pexpN/this.cp > 0.66){
+		  this.mode = 3;
+		  snCur = this.sn3 + pexp*Math.exp(((totaltime/1000)/this.tau(pexpN)));
+	   }else if(pexpN/this.cp > 0.20 ){
+		  this.mode = 2;
+		  snCur = this.sn2 + pexp*Math.exp(((totaltime/1000)/this.tau(pexpN)));
+	   }else if(pexpN/this.cp > 0.10 ){
+		  this.mode = 1;
+		  snCur = this.sn1 + pexp*Math.exp(((totaltime/1000)/this.tau(pexpN)));
+	  }else{
+		  snCur = this.sn + pexp*Math.exp(((totaltime/1000)/this.tau(pexpN)));
+	  }
 	  
-	  //this.sn = this.sn + pexp*(timediff/1000)*Math.exp(((totaltime/1000)*(1000/timediff)/this.tau()));
-	  this.sn = this.sn + pexp*Math.exp(((totaltime/1000)/this.tau()));
-	  //this.ln = Math.exp((-1*(totaltime/1000)*(1000/timediff)/this.tau()))*this.sn;
-	  this.ln = Math.exp((-1*(totaltime/1000)/this.tau()))*this.sn;
-		  
-	  return this.ln;
+	  var ln = Math.exp((-1*(totaltime/1000)/this.tau(pexpN)))*snCur;
+	  
+	  this.sn = ln/(Math.exp((-1*(totaltime/1000)/this.tau(pexpN,0))));
+	  this.sn1 = ln/(Math.exp((-1*(totaltime/1000)/this.tau(pexpN,1))));
+	  this.sn2 = ln/(Math.exp((-1*(totaltime/1000)/this.tau(pexpN,2))));
+	  this.sn3 = ln/(Math.exp((-1*(totaltime/1000)/this.tau(pexpN,3))));
+	  		  
+	  return ln;
   }
   
 }
