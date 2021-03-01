569328270e1b4ecd1a273c8c3635abf775dab6f8
jorgezwift <72138400+jorgezwift@users.noreply.github.com>
2021-03-01 09:20:54 +0100

    Update roadTime-logger.js

--- roadTime-logger.js
+++ roadTime-logger.js
@@ -1,35 +1,35 @@
-const ZwiftPacketMonitor = require('zwift-packet-monitor')
+const ZwiftPacketMonitor = require('./ZwiftPacketMonitor_tugaz')
+var ip = require("ip");
 
-var ifface = "192.168.1.92";
-// interface is cap interface name (can be device name or IP address)
-const monitor = new ZwiftPacketMonitor(ifface)
+const monitor = new ZwiftPacketMonitor(ip.address())
 
 
 
 
 var ended = false;
   
-monitor.on('outgoingPlayerState', (playerState, serverWorldTime) => {
-	//console.log("-----------------");
-	//console.log(serverWorldTime);
-	//console.log(playerState.distance);
-	//console.log(playerState.time);	
-	//console.log(playerState.power);		
-	console.log("id: "+playerState.id+"# distance: "+playerState.distance+" # roadTime: "+playerState.roadTime);
+monitor.on('outgoingPlayerState', (playerState, serverWorldTime) => {	
+			
+	var course =  ((playerState.f19 & 0xff0000) >> 16);
+	var world = course - 2;
+	var roadID = ((playerState.f20 & 0xff00) >> 8);
+	var isForward = ((playerState.f19 & 4) !== 0);
+	
+	console.log("id: "+playerState.id+"# distance: "+playerState.distance+" # roadTime: "+playerState.roadTime
+		   +" # world: "+world
+		   +" # roadID: "+roadID
+		   +" # isForward: "+isForward
+		   +" | roadTime: "+playerState.time);
   
 });
 
 
 
-monitor.on('incomingPlayerState', (playerState, serverWorldTime) => {
-	
-	
+monitor.on('incomingPlayerState', (playerState, serverWorldTime) => {	
 });
 
 
-// The Zwift server sends states in batches. This event is emitted at the end of each incoming batch
 monitor.on('endOfBatch', () => {
-  //console.log('end of batch')
 })
 
 monitor.start()
